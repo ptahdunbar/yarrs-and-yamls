6be2c5bea0f57938bcdf413d7a4775b860a8eed5
Ptah Dunbar <yarr@piratedunbar.com>
2015-07-06 13:27:57 -0400

    Copies both files on vagrant init

--- lib/vagrant-yarrs-and-yamls/plugin.rb
+++ lib/vagrant-yarrs-and-yamls/plugin.rb
@@ -9,7 +9,7 @@ module VagrantPlugins
 
             @@config = false
 
-            action_hook(:init, :environment_load) { maybe_create_yaml_file }
+            action_hook(:init, :environment_unload) { maybe_create_yaml_file }
             action_hook(:load_config, Plugin::ALL_ACTIONS) { load_config }
 
             action_hook(:require_vagrant_plugins, :environment_load) {
@@ -21,22 +21,28 @@ module VagrantPlugins
             def self.maybe_create_yaml_file
                 ARGV.each do |arg|
                     if arg.include?('init') || arg.include?('--yaml') || arg.include?('--yamls') || arg.include?('--yarrs')
+                        self.create_vagrant_file
                         self.create_yaml_file
                     end
                 end
             end
 
-            def self.create_yaml_file
-                source = __FILE__ + '/../../../example.Vagrantfile.yml'
-                source = File.realpath(source)
-                dest = Dir.pwd + '/Vagrantfile.yml'
-                self.backup_file(dest)
-                FileUtils.copy_file source, dest
+            def self.create_vagrant_file
+                source = __FILE__ + '/../../../example.Vagrantfile'
+                dest = Dir.pwd + '/Vagrantfile'
+                self.copy_file source, dest
             end
 
-            def self.backup_file(source)
-                dest = self.timestamp_filename(source)
-                FileUtils.mv source, dest, :force => true if File.exists? source
+            def self.create_yaml_file
+                source = __FILE__ + '/../../../example.Vagrantfile.yml'
+                dest = Dir.pwd + '/Vagrantfile.yml'
+                self.copy_file source, dest
+            end
+
+            def self.copy_file(source, dest)
+                source = File.realpath(source)
+                self.backup_file(dest)
+                FileUtils.copy_file source, dest
             end
 
             def self.timestamp_filename(file)
@@ -46,7 +52,6 @@ module VagrantPlugins
                 ext  = File.extname(file)
                 File.join(dir, "#{base}.bak.#{time}#{ext}")
             end
-
 
             def self.load_config
 
